64ba310a07df17bba6e6570736fd76f79b0d2e0c
Apple <apple@eduroam2-0417.funklan.uni-saarland.de>
2018-06-19 17:13:29 +0200

    Fix exec.sh

--- HW09/exec.sh
+++ HW09/exec.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ../../CRF++-0.58/crf_learn -c 10.0 template data-eng/eng.train model
-../../CRF++-0.58/crf_test  -m model data-eng/eng.dev
+../../CRF++-0.58/crf_test  -m model data-eng/eng.dev > output.txt
 
 #../../CRF++-0.58/crf_learn -a MIRA template data-eng/eng.train model
 #../../CRF++-0.58/crf_test  -m model data-eng/eng.dev
