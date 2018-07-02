45a2d6d8fa54c9fe4de04abeeb9f6f08a76e4b41
Carsten Klaus <s8caklau@stud.uni-saarland.de>
2018-07-02 13:50:14 +0200

    corrected bug and formated -g output

--- Project/utils.py
+++ Project/utils.py
@@ -26,11 +26,11 @@ def read_params():
     return args
 
 
-def print_members(args):
+def print_members():
     members = """
-        Janis Landwehr, 2547715, s9jaland@stud.uni-saarland.de
-        Sven Stauden, 2549696, s9svstau@stud.uni-saarland.de
-        Carsten Klaus, 2554140, s8caklau@stud.uni-saarland.de
+Janis Landwehr, 2547715, s9jaland@stud.uni-saarland.de
+Sven Stauden, 2549696, s9svstau@stud.uni-saarland.de
+Carsten Klaus, 2554140, s8caklau@stud.uni-saarland.de
         """
 
     print(members)
