8f2e95ff5b4ab15114f97ab2f0ba7e06d1b13692
Carsten Klaus <s8caklau@stud.uni-saarland.de>
2018-07-02 18:24:28 +0200

    add generaate_suffix_changing_rules

--- Project/task1.py
+++ Project/task1.py
@@ -6,6 +6,10 @@ def main():
 
     print(params)
 
+    #utils.read_file(params["train"])
+
+    utils.generate_suffix_changing_rules("__schielen","geschielt_")
+
 
 if __name__ == "__main__":
     main()
