b90fa5f7986b32a468f416ef8c91d3c9668e2fc5
Sven Stauden <s9svstau@stud.uni-saarland.de>
2018-07-29 15:57:09 +0200

    added some documentation

--- Project/implementation/task1.py
+++ Project/implementation/task1.py
@@ -4,6 +4,18 @@ from ChangingRule import SuffixRule, PrefixRule, RuleCollection
 from UniMorph import UniMorph, FeatureCollection
 
 def prepare_test_data(inflections):
+    """Creates out of a list of inlections three lists containing all lemmas, all feature lists and the expected inflection
+    
+    Parameters
+    ----------
+    inflections : List<Inflection>
+        List of inflection instances extracted from a dataset for which the data should be extracted
+    
+    Returns
+    -------
+    List<string>, List<FeatureCollection>, List<string>
+        3 lists containing all relevant information from the data
+    """
 
     test_lemmas = []
     test_feature_descs = []
@@ -17,6 +29,25 @@ def prepare_test_data(inflections):
     return test_lemmas, test_feature_descs, test_ground_truth
 
 def inflect_data(lemma_list, feature_desc_list, prefix_rule_col, suffix_rule_col):
+    """Applies learned rules in rule collections to a list of lemmas with corresponding FeatureCollections
+    
+    Parameters
+    ----------
+    lemma_list : List<string>
+        List of lemma strings that should be inflected
+    feature_desc_list : List<FeatureCollection>
+        List of FeatureCollection instances describing how the corresponding lemma should be inflected
+    prefix_rule_col : RuleCollection
+        RuleCollection instance containing all prefix rules that can be applied
+    suffix_rule_col : RuleCollection
+        RuleCollection instance containin all suffix rules that can be applied
+    
+    Returns
+    -------
+    List<string>
+        List of inflected lemma strings
+    """
+
 
     inflected_data = []
 
@@ -46,6 +77,23 @@ def inflect_data(lemma_list, feature_desc_list, prefix_rule_col, suffix_rule_col
 
 
 def compute_accuracy(predictions, ground_truth, verbose=False):
+    """Compares the ith prediction with the ith ground truth values and computes the overall accuracy.
+    
+    Parameters
+    ----------
+    predictions : List<string>
+        List containing all predicted inflections
+    ground_truth : List<string>
+        List containing all real (estimated) inflections
+    verbose : bool, optional
+        If True, prints for each prediction, the ground truth (the default is False, which)
+    
+    Returns
+    -------
+    Float
+        Final accuracy value indicating how many samples are corectly predicted
+    """
+
 
     total_count = len(predictions)
     correct = 0
@@ -75,50 +123,20 @@ def main():
     train_inflections = utils.read_file(params["train"])
     prefix_rule_collection, suffix_rule_collection = RuleCollection.create_rule_collections(train_inflections)
 
+    # Create test set
     test_inflections = utils.read_file(params["test"])
     test_lemmas, test_feature_descs, test_ground_truth = prepare_test_data(test_inflections)
 
+    # apply rules on test data
     predictions = inflect_data(test_lemmas, test_feature_descs, prefix_rule_collection, suffix_rule_collection)
 
+    # compute the accuracy
     acc = compute_accuracy(predictions, test_ground_truth, verbose=True)
 
+    # print the final results
     print("Training Samples: {}".format(len(train_inflections)))
     print("Test Samples: {}".format(len(test_inflections)))
     print("Accuracy: {}%".format(acc * 100))
-    
-
-    # # Create testing data
-
-    # lemma = "verkaufen"
-    # features_string = "V.PTCP;PST"
-
-    # feature_col = FeatureCollection.create_feature_collection(features_string)
-
-    # best_suffix_rule = suffix_rule_collection.get_highest_overlap_rule(lemma, feature_col)
-
-    # # use empty rule if no rule matches
-    # if best_suffix_rule is None:
-    #     best_suffix_rule = SuffixRule.empty_rule(feature_col)
-
-    # best_prefix_rule = prefix_rule_collection.get_highest_count_rule(lemma, feature_col)
-
-    # # use empty rule if no rule matches
-    # if best_prefix_rule is None:
-    #     best_prefix_rule = PrefixRule.empty_rule(feature_col)
-
-    # inflected_lemma = best_suffix_rule.apply_rule(lemma)
-    # inflected_lemma = best_prefix_rule.apply_rule(inflected_lemma)
-
-    # print(best_suffix_rule)
-    # print(prefix_rule_collection)
-    # print(inflected_lemma)
-
-    
-
-    # perfix1, stem1, suffix1 = apply_most_overlapping_suffix_rule(lemma, feature_col)
-    # perfix2, stem2, suffix2 = apply_most_overlapping_prefix_rule(lemma, feature_col)
-    # final = prefix2, stem1, suffix1
-
 
 if __name__ == "__main__":
     main()
